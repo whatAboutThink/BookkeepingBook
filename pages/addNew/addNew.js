969f5a89e6c38279703ec3cef58815898f269779
Engt <13640282804@163.com>
2023-05-30 23:54:41 +0800

    提交小程序代码

--- pages/addNew/addNew.js
+++ pages/addNew/addNew.js
@@ -1,5 +1,6 @@
 // pages/addNew/addNew.js
-import { $wuxToast } from '../../miniprogram_npm/wux-weapp/index'
+import { $wuxToast } from '../../miniprogram_npm/wux-weapp/index';
+import { $wuxDialog } from '../../miniprogram_npm/wux-weapp/index';
 Page({
 
   /**
@@ -178,7 +179,27 @@ Page({
             success: () => console.log('已完成'),
           })
     }
-
-    
+  },
+  save:function(){
+    if (this.timeout) clearTimeout(this.timeout)
+    const hideDialog = $wuxDialog().open({
+        resetOnClose: true,
+        title: '三秒后自动关闭',
+        content:
+          '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
+        buttons: [
+          {
+            text: '取消',
+          },
+          {
+            text: '确定',
+            type: 'primary',
+            onTap(e) {
+              console.log(e)
+            },
+          },
+        ],
+      })
+      this.timeout = setTimeout(hideDialog, 3000)
   }
 })
